c0f763f75c039d096d0a458a44a7b678d1f8d07a
ics-sawada-h <haruka.sawada@ics-web.jp>
2024-08-19 19:56:44 +0900

    パスの修正

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,12 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   build: {
+    outDir: "../dist",
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
         sample: resolve(__dirname, "src/sample/index.html"),
         multiple: resolve(__dirname, "src/multiple-animation/index.html"),
-      },
-      output: {
-        chunkFileNames: "assets/[name].js",
-        assetFileNames: "assets/[name][extname]",
       },
     },
     base: "/240827_theatrejs/",
